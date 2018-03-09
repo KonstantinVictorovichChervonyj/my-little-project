e548e558d1135db0359be00ff2ba753390c69d94
Konstantin <kostyachervonyj@gmail.com>
2018-03-09 22:38:23 +0200

    yoga-project

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    gulp.src("./sass/**/*.sass")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("./css"));
+    });
+gulp.task('watch', function(){
+    gulp.watch("./sass/**/*.sass", ["sass"]);
+});
